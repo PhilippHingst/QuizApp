f61d20b0d31d5d0bd17cb2c7ee111f94c8bc7388
PhilippHingst <hingst.philipp@web.de>
2022-10-19 15:35:40 +0200

    added All

---
+++ json.js
@@ -0,0 +1,66 @@
+let questions = [
+    {
+        "question": "Wer hat HTML erfunden?",
+        "answer_1": "Robbie Williams",
+        "answer_2": "Lady Gaga",
+        "answer_3": "Tim Berners-Lee",
+        "answer_4": "Justin Bieber",
+        "right_answer": 3
+    },
+    {
+        "question": "Wie heißt die Hauptstadt von Spanien?",
+        "answer_1": "London",
+        "answer_2": "Madrid",
+        "answer_3": "Berlin",
+        "answer_4": "Lissabon",
+        "right_answer": 2
+    },
+    {
+        "question": "Was transportierte die erste Eisenbahn in Deutschland?",
+        "answer_1": "Zwei Fässer Bier",
+        "answer_2": "Personen",
+        "answer_3": "4 Kühe",
+        "answer_4": "Nichts",
+        "right_answer": 1
+    },
+    {
+        "question": "Wer hat das erste Auto erfunden?",
+        "answer_1": "Abraham Lincoln",
+        "answer_2": "James Bowman Lindsay",
+        "answer_3": "Nicolaus Otto",
+        "answer_4": "Carl Benz",
+        "right_answer": 4
+    },
+    {
+        "question": "Wie viele Länder sind in der EU 2022?",
+        "answer_1": "28",
+        "answer_2": "41",
+        "answer_3": "27",
+        "answer_4": "36",
+        "right_answer": 3
+    },
+    {
+        "question": "Wie viele Länder sind in der EU 2022?",
+        "answer_1": "28",
+        "answer_2": "41",
+        "answer_3": "27",
+        "answer_4": "36",
+        "right_answer": 3
+    },
+    {
+        "question": "Wie viele Länder sind in der EU 2022?",
+        "answer_1": "28",
+        "answer_2": "41",
+        "answer_3": "27",
+        "answer_4": "36",
+        "right_answer": 3
+    },
+    {
+        "question": "Wie viele Länder sind in der EU 2022?",
+        "answer_1": "28",
+        "answer_2": "41",
+        "answer_3": "27",
+        "answer_4": "36",
+        "right_answer": 3
+    },
+];
---
+++ script.js
@@ -0,0 +1,103 @@
+let rightQuestions = 0;
+let currentQuestion = 0;
+let AUDIO_SUCCESS = new Audio('sound/right.wav');
+let AUDIO_FAIL = new Audio('sound/wrong.wav');
+let AUDIO_ENDSCREEN = new Audio('sound/endscreen.wav');
+
+function init() {
+    document.getElementById('lastQuestion').innerHTML = questions.length;
+    if (currentQuestion >= questions.length) {
+        document.getElementById('currentQuestionEndscreen').innerHTML = currentQuestion;
+        document.getElementById('rightAnswers').innerHTML = rightQuestions;
+    } else {
+        document.getElementById('currentQuestion').innerHTML = currentQuestion + 1;
+        showQuestion();
+    }
+}
+
+function showQuestion() {
+    if (gameIsOver()) {// TODO: Show Endscreen   
+        showEndScreen();
+    } else { // show Questions
+        updateProgressBar();
+        let question = questions[currentQuestion];
+        showQuestionAndAnswer(question);
+    }
+}
+
+function gameIsOver() {
+    return currentQuestion >= questions.length;
+}
+
+function showEndScreen() {
+    document.getElementById('endScreen').style = "";
+    document.getElementById('questionScreen').style = "display:none";
+    AUDIO_ENDSCREEN.play();
+}
+
+function updateProgressBar() {
+    let percent = (currentQuestion + 1) / questions.length;
+    percent = Math.round(percent * 100); // Math.round rundet das Ergebnis
+    document.getElementById('progress-bar').innerHTML = `${percent}%`;
+    document.getElementById('progress-bar').style = `width: ${percent}%;`;
+
+}
+
+function showQuestionAndAnswer(question) {
+    document.getElementById('questiontext').innerHTML = question['question'];
+    document.getElementById('answer_1').innerHTML = question['answer_1'];
+    document.getElementById('answer_2').innerHTML = question['answer_2'];
+    document.getElementById('answer_3').innerHTML = question['answer_3'];
+    document.getElementById('answer_4').innerHTML = question['answer_4'];
+}
+
+function answer(selection) {
+    let question = questions[currentQuestion];
+    let selectedQuestionNumber = selection.slice(-1);
+    let idOfRightAnswer = `answer_${question['right_answer']}`;
+
+    if (selectedQuestionNumber == question['right_answer']) { // Right Answer
+        showRightAnswer(selection);
+    } else {// Wrong Answer
+        showWrongAnswer(selection, idOfRightAnswer);
+
+    }
+    document.getElementById('next-button').disabled = false;
+
+}
+
+function showRightAnswer(selection) {
+    document.getElementById(selection).classList.add('bg-success');
+    AUDIO_SUCCESS.play();
+    rightQuestions++;
+}
+
+function showWrongAnswer(selection, idOfRightAnswer) {
+    document.getElementById(selection).classList.add('bg-danger');
+    document.getElementById(idOfRightAnswer).classList.add('bg-success');
+    AUDIO_FAIL.play();
+}
+
+function nextQuestion() {
+    currentQuestion++; // z.B von 0 auf 1
+    resetAnswerButtons();
+    showQuestion();
+    init();
+    document.getElementById('next-button').disabled = true;
+}
+
+function resetAnswerButtons() {
+    document.getElementById('answer_1').classList.remove('bg-success', 'bg-danger');
+    document.getElementById('answer_2').classList.remove('bg-success', 'bg-danger');
+    document.getElementById('answer_3').classList.remove('bg-success', 'bg-danger');
+    document.getElementById('answer_4').classList.remove('bg-success', 'bg-danger');
+}
+
+function restartGame() {
+    document.getElementById('endScreen').style = "display:none";
+    document.getElementById('questionScreen').style = "";
+    rightQuestions = 0;
+    currentQuestion = 0;
+    init();
+}
+
